7bf35bd9fac3c7e25a5e6b44a08347dd31657aa0
Kunal Patil <kunalpatil970730@gmail.com>
2023-03-29 18:34:52 +0530

    commit

--- myvenv/Scripts/activate.bat
+++ myvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Kunal\Project\Clg\Minor\Django_Working\NECBDC\myvenv
+set VIRTUAL_ENV=C:\Kunal\Project\College\Minor\Django_Working\Necbdc clone\NECBDC\myvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
